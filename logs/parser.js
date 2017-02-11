d0bd1195b52be90b2d3fe693255e24e3fef8066a
Hekit <kubondavid@seznam.cz>
2017-02-11 03:40:51 +0100

    logging tools

---
+++ logs/parser.js
@@ -0,0 +1,23 @@
+//var regex = require('regex');
+var database = require('./database');
+
+var db = new database();
+
+function parser (data) {
+    data.toString().split('\n').forEach(function (line) {
+        parseLine(line, addLine);
+    });
+}
+
+var parseLine = function (line, callback) {
+    var ip = line.substr(0, line.indexOf(' '));
+    var time = line.substr(line.indexOf('[')+1, line.indexOf(']')-line.indexOf('[')-1);
+    var request = line.substr(line.indexOf('"'));
+    callback([ip, time, request]);
+}
+
+var addLine = function (line) {
+    db.insert(line[0], line[1], line[2]);
+}
+
+module.exports = parser;
