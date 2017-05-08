e80452d7a3c9c654ac820ca6b1d5522ecbca4c56
Hekit <kubondavid@seznam.cz>
2017-05-08 10:37:54 +0200

    database setup shell script

---
+++ database-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+dropdb -p 5433 lindat-billing-test
+createdb -p 5433 lindat-billing-test
+psql -p 5433 lindat-billing-test << EOF
+
+CREATE SEQUENCE logs_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    MAXVALUE 10000000
+    CACHE 1;
+
+CREATE TABLE Users
+(
+id_u bigint NOT NULL DEFAULT nextval('logs_id_seq'::regclass),
+ip inet,
+CONSTRAINT users_pkey PRIMARY KEY (id_u)
+);
+
+
+CREATE TABLE Logs
+(
+id_l SERIAL PRIMARY KEY,
+id_u int,
+id_s int,
+datetime timestamp with time zone,
+request text,
+CONSTRAINT logs_pkey PRIMARY KEY (id_l)
+)
+
+\q
+EOF
