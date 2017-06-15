69b4f6f5436d8d1d0a8dbb7678fc0aad1bae6fd0
Hekit <kubondavid@seznam.cz>
2017-06-15 18:21:02 +0200

    minor changes

--- server/log_management/parser.js
+++ server/log_management/parser.js
@@ -15,11 +15,16 @@ var db = new database();
 function parser (data) {
     return new Promise((resolve, reject) => {
         var lines = [];
+        var logEntry;
         //console.log(data.toString());
         data.toString().split('\n').forEach(function (line, idx, array) {
             //console.log(lines);
             if (line.length > 0) {
-                if (idx !== array.length-1) lines.push(parseLine(line));
+                if (idx !== array.length-1) {
+                    logEntry = parseLine(line);
+                    if (logEntry.service)
+                        lines.push(logEntry);
+                }
                 else {
                     //console.log("last line length ", Buffer.byteLength(line,'utf-8'));
                     if (lines.length > 0) {
@@ -42,8 +47,10 @@ var parseLine = (line) => {
     var request = line.substr(line.indexOf('"'));
     var service = false;
     if (request.substr(1,3) == "GET") {
-        var arrRequest = request.substr(5).split(['/']);
+        /*var arrRequest = request.substr(5).split(['/']);
         if (arrRequest[1] == "services")
+            service = true;*/
+        if (request.indexOf('/services/') !== -1)
             service = true;
     }
 
