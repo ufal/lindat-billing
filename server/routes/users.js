0486571de97c92e2287bd733af4985a2954a4c96
Hekit <kubondavid@seznam.cz>
2017-06-25 17:39:23 +0200

    logging minor fix

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,15 +2,16 @@
  * Backend of the user-to-data-access showing page
  */
 
-var express = require('express');
-var router = express.Router();
-var fs = require('fs');
-var url = require('url');
-var database = require('../log_management/database');
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const url = require('url');
+const database = require('../log_management/database');
+const logger = require('winston');
 
-var logs = [];
+let logs = [];
 
-var db = new database();
+const db = new database();
 
 //log_management = parser('195.113.20.155 - - [08/Mar/2016:14:48:43 +0100] "GET /Shibboleth.sso/DiscoFeed HTTP/1.1" 200 389353 "-" "Java/1.8.0_45"');
 //db.insert();
@@ -18,19 +19,19 @@ var db = new database();
 
 router.get('/users/*', function (req, res, next) {
     // select relevant data from database
-    var request = req.url.split(['/']);
-    var id = request[2];
-    var from = request[3].substr(5);
-    var to = request[4].substr(3);
+    const request = req.url.split(['/']);
+    const id = request[2];
+    const from = request[3].substr(5);
+    const to = request[4].substr(3);
     if (validateIPaddress(id)) {
         logger.debug("SELECT * from logs WHERE ip = '" + id + "'::inet and datetime > to_timestamp('"
             + from + "', 'DD-MM-YYYY') and datetime <= to_timestamp('" + to + "', 'DD-M-YYYY')");
         db.select(" WHERE ip = '" + id + "'::inet and datetime > to_timestamp('" + from
             + "', 'DD-MM-YYYY') and datetime <= to_timestamp('" + to + "', 'DD-M-YYYY')")
             .then(data => {
-                var dataModified = [];
-                var empty = true;
-                for(var i = 0; i < db.getServiceCount(); i++) {
+                let dataModified = [];
+                let empty = true;
+                for(let i = 0; i < db.getServiceCount(); i++) {
                     dataModified.push({
                         name: db.getServiceName(i+1),
                         logData: [],
