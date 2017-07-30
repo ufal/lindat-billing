4b0a5ef73e7c87d9c873eca9c3b297552887864b
Hekit <kubondavid@seznam.cz>
2017-07-30 23:56:07 +0200

    registration added

--- server/routes/login.js
+++ server/routes/login.js
@@ -15,32 +15,54 @@ router.post('/authenticate', function (req, res, next) {
     logger.debug('Authenticating', req.body);
     db.authenticate(req.body.username, req.body.password)
         .then(data => {
-            logger.debug('Retrieved user', data);
-            const token = jwt.sign({
-                username: req.body.username,
-                admin: true
-            },
-                'secret',
-                { expiresIn: 15 }
-                );
-            logger.debug('Password correct');
-            logger.info('Issued token');
-            res.status(200);
-            res.json({
-                success: true,
-                message: 'Enjoy the token!',
-                token: token
-            });
+            onSuccess(data, res);
         })
         .catch(error => {
-            console.log(error);
-            //res.send(error);
-            res.status(401);
-            res.json({
-                success: false,
-                message: error
-            });
+            onFailure(error, res);
         });
 });
+
+router.post('/accounts', function (req, res, next) {
+    logger.debug('Adding new user', req.body.username);
+    db.addAccount(req.body.username, req.body.password)
+        .then(data => {
+            onSuccess(data, res);
+        })
+        .catch(error => {
+            onFailure(error, res);
+        });
+});
+
+onSuccess = (data, res) => {
+    const token = getToken(data);
+    logger.info('Issued token');
+    res.status(200);
+    res.json({
+        success: true,
+        message: 'Enjoy the token!',
+        token: token
+    });
+};
+
+onFailure = (error, res) => {
+    console.log(error);
+    //res.send(error);
+    res.status(401);
+    res.json({
+        success: false,
+        message: error
+    });
+}
+
+getToken = (data) => {
+    const token = jwt.sign({
+            username: data[0].username,
+            admin: data[0].admin
+        },
+        'secret',
+        { expiresIn: 15 }
+    );
+    return token;
+};
 
 module.exports = router;
