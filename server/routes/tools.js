948daa15db1dc0013ca66158a539a75388b3f5f1
Hekit <kubondavid@seznam.cz>
2017-08-09 18:00:38 +0200

    minor fix

--- server/routes/tools.js
+++ server/routes/tools.js
@@ -1,9 +1,11 @@
 /**
  * Backend tools needed by the frontend
  */
-const router = require('express').Router;
+const router = require('express').Router();
 const version = require('../../package.json').version;
 
 router.get('/version', function (req, res, next) {
     res.json(version);
 });
+
+module.exports = router;
