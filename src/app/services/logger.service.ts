7c5b0e5623a0b942c58cf47f4ea67eaf8599ddad
Hekit <kubondavid@seznam.cz>
2017-08-22 17:36:05 +0200

    angular logging fix

--- src/app/services/logger.service.ts
+++ src/app/services/logger.service.ts
@@ -1,9 +1,10 @@
 import { Injectable }   from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class LoggerService{
+
     constructor(private http:Http) {
     }
 
