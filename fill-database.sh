c48feb7c251502fde6c53910d07f3a0ef7e1c22b
Hekit <kubondavid@seznam.cz>
2017-08-29 17:42:12 +0200

    dev setup

---
+++ fill-database.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+psql -p 5433 -q -v "ON_ERROR_STOP=1" lindat-billing-test << EOF
+
+INSERT INTO Accounts (username, pass, admin) VALUES('david', crypt('heslo', gen_salt('bf')), true);
+INSERT INTO Accounts (username, pass, admin) VALUES('jm', crypt('heslo', gen_salt('bf')), true);
+
+INSERT INTO Accounts (username, pass, admin) VALUES('user', crypt('heslo', gen_salt('bf')), false);
+INSERT INTO Accounts (username, pass, admin) VALUES('honza', crypt('heslo', gen_salt('bf')), false);
+INSERT INTO Accounts (username, pass, admin) VALUES('johny', crypt('heslo', gen_salt('bf')), false);
+
+
+INSERT INTO Logins VALUES('user', '1502809718020');
+INSERT INTO Logins VALUES('user', '1502809718021');
+INSERT INTO Logins VALUES('user', '1502809718022');
+INSERT INTO Logins VALUES('honza', '1502809718025');
+
+
+INSERT INTO Users (ip,owner) VALUES('127.0.0.1', 'user');
+INSERT INTO Users (ip,owner) VALUES('127.0.0.2', 'user');
+INSERT INTO Users (ip,owner) VALUES('127.0.0.10', 'honza');
+INSERT INTO Users (ip,owner) VALUES('127.0.0.11', 'honza');
+INSERT INTO Users (ip,owner) VALUES('127.0.0.12', 'honza');
+INSERT INTO Users (ip,owner) VALUES('127.0.0.20', 'johny');
+INSERT INTO Users (ip,owner) VALUES('127.0.0.21', 'johny');
+
+
+\q
+EOF
+
+if [ $? -gt 0 ];
+then "Error in database fillling"
+else
+    echo "Database is filled with test data"
+fi
