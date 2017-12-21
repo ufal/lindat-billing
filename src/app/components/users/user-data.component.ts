b6e4be481d805a3cd7cebd21e954ba7a4e928fb9
Hekit <kubondavid@seznam.cz>
2017-12-21 16:28:55 +0100

    user data visualization controls update
    Added buttons for the most common recent periods and a respective function for handling them. Changed the layout of the buttons, but the correct layout remains an issue. #1748

--- src/app/components/users/user-data.component.ts
+++ src/app/components/users/user-data.component.ts
@@ -1,58 +1,54 @@
-import { Component } from '@angular/core';
-import { IMyOptions, IMyDateRangeModel, IMyDateRange, IMyDateSelected, IMyCalendarViewChanged, IMyInputFieldChanged } from 'mydaterangepicker';
-import { UserDataService, AlertService, LoggerService, AuthenticationService } from '../../services/index';
+import { Component } from "@angular/core";
+import { IMyDateRange, IMyDateRangeModel, IMyOptions } from "mydaterangepicker";
+import { AlertService, AuthenticationService, LoggerService, UserDataService } from "../../services/index";
 
 @Component({
     moduleId: module.id,
-    selector: 'user-data',
-    templateUrl: 'user-data.component.html',
-    styleUrls: [ 'user-data.component.css' ],
+    selector: "user-data",
+    styleUrls: [ "user-data.component.css" ],
+    templateUrl: "user-data.component.html",
 })
 
 export class UserDataComponent  {
-    title: string;
-    data: Object[];
-    errorMessage: string;
-    isAdmin: boolean;
+    protected title: string;
+    protected data: Object[];
+    protected errorMessage: string;
+    protected isAdmin: boolean;
 
-    model: IMyDateRange = {
+    protected model: IMyDateRange = {
         beginDate: {year: new Date().getFullYear(), month: new Date().getMonth(), day: new Date().getDate()},
-        endDate: {year: new Date().getFullYear(), month: new Date().getMonth() + 1, day: new Date().getDate()}
+        endDate: {year: new Date().getFullYear(), month: new Date().getMonth() + 1, day: new Date().getDate()},
     };
-    placeholderTxt: string = 'Insert timespan';
+    protected readonly placeholderTxt: string = "Insert timespan";
 
     constructor(
         private userDataService: UserDataService,
         private alertService: AlertService,
         private loggerService: LoggerService,
-        private authenticationService: AuthenticationService
+        private authenticationService: AuthenticationService,
     ) {
         this.isAdmin = authenticationService.isAdmin();
     }
 
-    onClickMe() {
-        //this.searchUser();
-    }
-
     // expanding details of a service summary
-    expand(p: any) {
+    protected expand(p: any) {
         p.expanded = !p.expanded;
     }
 
     // debug only - testing data
-    test(ip: string, from: string, to: string) {
-        console.log('Showing prepared sample');
+    protected test(ip: string, from: string, to: string) {
+        console.log("Showing prepared sample");
         this.title = ip;
         this.getData(ip, from, to);
     }
 
     // prepare information to get data of a specific user
-    searchUser(event: Event) {
-        event.preventDefault();
+    protected searchUser(/*event: Event*/) {
+        //event.preventDefault();
 
         if (typeof this.model === undefined || !this.model)
         {
-            this.errorMessage = 'The dates you entered are not valid. Check them and try again.';
+            this.errorMessage = "The dates you entered are not valid. Check them and try again.";
             this.data = [];
             return;
         }
@@ -66,17 +62,17 @@ export class UserDataComponent  {
     }
 
     //  get data of a specific user
-    getData(ip: string, from: string, to: string)
+    protected getData(ip: string, from: string, to: string)
     {
         this.userDataService.getUser(ip, from, to)
-            .subscribe(log => {
-                if (log[0] == "ERROR") {
+            .subscribe((log) => {
+                if (log[0] === "ERROR") {
                     this.alertService.error(log[1]);
                     this.data = [];
-                    this.loggerService.log('debug',log[1]);
-                } else if (log[0] == "EMPTY") {
-                    let message = 'User ' + this.title
-                        + ' has no relevant data for the period between ' + from + ' and ' + to;
+                    this.loggerService.log("debug", log[1]);
+                } else if (log[0] === "EMPTY") {
+                    let message = "User " + this.title
+                        + " has no relevant data for the period between " + from + " and " + to;
                     this.alertService.error(message);
                     this.data = [];
                 } else {
@@ -88,39 +84,58 @@ export class UserDataComponent  {
             });
     }
 
+    protected lastPeriod(what: string)
+    {
+        let now = new Date();
+
+        switch (what)
+        {
+            case "month" :
+                now.setMonth(now.getMonth() - 1);
+                break;
+            case "week" :
+                now.setDate(now.getDate() - 7);
+                break;
+            case "day"  :
+                now.setDate(now.getDate() - 1);
+                break;
+            default :
+        }
+
+        const from = now.getDate() + "-" + (now.getMonth() + 1) + "-" + now.getFullYear();
+        const to = new Date().getDate() + "-" + (new Date().getMonth() + 1) + "-" + new Date().getFullYear();
+
+        this.getData(this.title, from, to);
+    }
+
     // get the starting date of a period
-    getStart = () => {
+    private getStart() {
         const date = this.model.beginDate;
         return date.day + "-" + date.month + "-" + date.year;
-    };
+    }
 
     // get the ending date of a period
-    getEnd() {
+    private getEnd() {
         const date = this.model.endDate;
         return date.day + "-" + date.month + "-" + date.year;
     }
 
     // daterangepicker options
-    private myDateRangePickerOptions: IMyOptions = {
-        quickRangeSelect: true,
-        dateFormat: 'dd.mm.yyyy',
-        showClearBtn: false,
+    protected  myDateRangePickerOptions: IMyOptions = {
         acceptBtnTxt: "Apply",
-        markCurrentDay: true,
-        minYear: 2015,
-        maxYear: new Date().getFullYear(),
+        dateFormat: "dd.mm.yyyy",
         inline: false,
+        markCurrentDay: true,
+        maxYear: new Date().getFullYear(),
+        minYear: 2015,
+        quickRangeSelect: true,
+        showClearBtn: false,
         showClearDateRangeBtn: false,
     };
 
     // dates changed
-    onDateRangeChanged(event: IMyDateRangeModel) {
+    protected onDateRangeChanged(event: IMyDateRangeModel) {
         this.model = {beginDate: event.beginDate, endDate: event.endDate};
-        console.log('onDateRangeChanged(): ', event.formatted, 'with model', this.model);
-    }
-
-    clearDateRange() {
-        this.model = null;
-        console.log('DateRange Cleared');
+        console.log("onDateRangeChanged(): ", event.formatted, "with model", this.model);
     }
 }
