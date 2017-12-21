814e3be4068257b01526003f37b8d05ac3d899c9
Hekit <kubondavid@seznam.cz>
2017-12-21 16:42:46 +0100

    tslint refactoring

--- src/app/components/accounts-management/accounts-management.component.ts
+++ src/app/components/accounts-management/accounts-management.component.ts
@@ -1,36 +1,36 @@
-import { Component } from '@angular/core';
-import { AuthenticationService, AccountDataService, AlertService } from '../../services/index';
+import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import { AccountDataService, AlertService, AuthenticationService } from "../../services/index";
 
 @Component({
     moduleId: module.id,
-    selector: 'accounts-management',
-    templateUrl: 'accounts-management.component.html',
-    styleUrls: ['accounts-management.component.css']
+    selector: "accounts-management",
+    styleUrls: ["accounts-management.component.css"],
+    templateUrl: "accounts-management.component.html",
 })
 
 export class AccountsManagementComponent {
 
-    isAdmin: boolean;
-    username: string;
-    data: Object[];
-    ipToAdd: string;
+    protected isAdmin: boolean;
+    protected username: string;
+    protected data: Object[];
+    protected ipToAdd: string;
 
     constructor(
         private router: Router,
         private authenticationService: AuthenticationService,
         private alertService: AlertService,
-        private accountDataService: AccountDataService
+        private accountDataService: AccountDataService,
     ) {
         this.isAdmin = authenticationService.isAdmin();
         this.username = this.authenticationService.getUsername();
         this.getAllAccounts();
     }
 
-    getAllAccounts() {
+    protected getAllAccounts() {
         this.accountDataService.getAll()
-            .subscribe(data => {
-                if (data[0] == "ERROR") {
+            .subscribe((data) => {
+                if (data[0] === "ERROR") {
                     this.alertService.error(data[1]);
                     this.data = [];
                 } else {
@@ -40,37 +40,37 @@ export class AccountsManagementComponent {
             });
     }
 
-    addNewIP() {
+    protected addNewIP() {
         this.accountDataService.addNewIP(this.ipToAdd, this.username)
-            .subscribe(message => {
-                if (message.toString() == "ERROR") {
-                    this.alertService.error('Failed to add IP!');
-                } else if (message.toString() == "SUCCESS") {
-                    this.alertService.success('New IP successfully added!');
+            .subscribe((message) => {
+                if (message.toString() === "ERROR") {
+                    this.alertService.error("Failed to add IP!");
+                } else if (message.toString() === "SUCCESS") {
+                    this.alertService.success("New IP successfully added!");
                 } else {
-                    this.alertService.error('We are sorry but something unexpected just happened');
+                    this.alertService.error("We are sorry but something unexpected just happened");
                 }
             });
     }
 
-    reportIP() {
+    protected reportIP() {
         this.accountDataService.reportIP(this.ipToAdd, this.username)
-            .subscribe(message => {
-                if (message.toString() == "ERROR") {
-                    this.alertService.error('Failed to report IP!');
-                } else if (message.toString() == "SUCCESS") {
-                    this.alertService.success('The IP was successfully reported!');
+            .subscribe((message) => {
+                if (message.toString() === "ERROR") {
+                    this.alertService.error("Failed to report IP!");
+                } else if (message.toString() === "SUCCESS") {
+                    this.alertService.success("The IP was successfully reported!");
                 } else {
-                    this.alertService.error('Not yet implemented');
+                    this.alertService.error("Not yet implemented");
                 }
             });
     }
 
-    changePassword() {
-        this.alertService.error('Not yet implemented');
+    protected changePassword() {
+        this.alertService.error("Not yet implemented");
     }
 
-    expand(p: any) {
+    protected expand(p: any) {
         p.expanded = !p.expanded;
     }
 }
