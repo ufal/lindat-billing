bfc29557f44cb73a3d3bfbe5a01c04e75da6a31c
Hekit <kubondavid@seznam.cz>
2018-01-11 17:06:12 +0100

    refactoring and cleanup

--- src/app/components/users/user-data.component.ts
+++ src/app/components/users/user-data.component.ts
@@ -1,7 +1,6 @@
 import { Component } from "@angular/core";
 import { IMyDateRange, IMyDateRangeModel, IMyOptions } from "mydaterangepicker";
 import { AlertService, AuthenticationService, LoggerService, UserDataService } from "../../services/index";
-import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
     moduleId: module.id,
