a66c728913fad650576c3e6026a7a71cbfb5eb48
Hekit <kubondavid@seznam.cz>
2018-05-18 12:10:34 +0200

    pricing +- update

--- server/routes/pricing.js
+++ server/routes/pricing.js
@@ -12,9 +12,18 @@ router.post('/pricing', function (req, res, next) {
     let username = req.body.name;
     let body = req.body.data;
     if (username === '') username = 'default';
-    logger.debug(body);
-
-    db.setPricing(username, body);
+    body = JSON.parse(body);
+    let json = "{\n";
+    let data = [];
+    for(let item in body) {
+        logger.debug(body[item]);
+        data.push({item: {"name": body[item].name, "value": body[item].value }});
+        if (item > 0) json += ",\n";
+        json += "\"" + body[item].id + "\": {\"name\": \"" + body[item].name + "\", \"value\": " + body[item].value + "}";
+    }
+    json += "\n}";
+    logger.debug(json);
+    db.setPricing(username, json);
     /*    .then(() => {
             res.json();
         })
@@ -31,8 +40,9 @@ router.get('/pricing/*', function (req, res, next) {
     db.getPricing(id)
         .then(prices => {
             let data = [];
-            for(let item in prices) {
-                if (item > 0) data.push(prices[item]);
+            for(let id in prices) {
+                prices[id].id = id;
+                data.push(prices[id]);
             }
             res.json(data);
         })
